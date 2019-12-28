859202cbded2ad69c2bd189ff01e52ddc6a61ba8
hemanolisamp <monikaputri299@gmail.com>
2019-12-28 19:05:07 +0700

    1718027_profile_bahasa

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -1,9 +1,13 @@
-
 package com.example.tugas1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.tugas1.Setting.AppSeting;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void showsetting(){
+        setContentView(R.layout.activity_setting);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode){
+        switch (selectedMode){
+            case R.id.bahasa:
+                Intent setting = new Intent(MainActivity.this, AppSeting.class);
+                startActivity(setting);
+                return;
+        }
     }
 }
