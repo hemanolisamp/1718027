70b69146e839eb2bbeb4f3448e98399ae2c926c0
hemanolisamp <monikaputri299@gmail.com>
2019-12-31 13:06:06 +0700

    1718027_setting_bahasa_diprofil

--- app/src/main/java/com/example/tugas1/MainActivity.java
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -2,42 +2,31 @@ package com.example.tugas1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.tugas1.Setting.AppSeting;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-public class MainActivity extends AppCompatActivity {
+    ImageButton seting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seting = findViewById(R.id.setting);
+        seting.setOnClickListener(this);
+    }
 
-    }
-    public void showsetting(){
-        setContentView(R.layout.activity_setting);
-    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.settings, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    public void onClick(View look) {
 
-    public boolean onOptionsItemSelected(MenuItem item){
-        setMode(item.getItemId());
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void setMode(int selectedMode){
-        switch (selectedMode){
-            case R.id.bahasa:
-                Intent setting = new Intent(MainActivity.this, AppSeting.class);
-                startActivity(setting);
-                return;
+        if(look.getId()==R.id.setting){
+            Intent intent = new Intent (Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
         }
     }
 }
